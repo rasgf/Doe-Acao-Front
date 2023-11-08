347df0ec4bb51b54fc2045bcf1500ffe4eb7ed28
Nathan Gabriel <nathancoelho.job@gmail.com>
2023-11-08 12:18:32 -0300

    Colors

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -40,7 +40,7 @@ function Login() {
                             >
                                 <input
                                     type="checkbox"
-                                    className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-pink-500 checked:bg-pink-500 checked:before:bg-pink-500 hover:before:opacity-10"
+                                    className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-rose-500 checked:bg-rose-500 checked:before:bg-rose-500 hover:before:opacity-10"
                                     id="checkbox"
                                 />
                                 <span className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
@@ -62,7 +62,7 @@ function Login() {
                             </label>
                             <label
                                 className="mt-px cursor-pointer select-none font-light text-gray-700"
-                                htmlFor="checkbox"
+                                htmlFor="checkbox "
                             >
                                 Lembre-me
                             </label>
