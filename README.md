b55764eb92e71793156a4a02097750a8bb878d60
Nathan Gabriel <nathancoelho.job@gmail.com>
2023-11-08 10:56:18 -0300

    Add README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Doe-Acao-Front
-Front-end do Projeto Integrador
+
+Doe Ação é uma rede social com o propósito de conectar pessoas com ONGs que necessitam de ajuda. Nessa rede, as ONGs publicam postagens a partir de um tema, como, por exemplo, trabalho voluntário, doação de alimentos, roupas ou brinquedos, etc. Os usuários conseguem se conectar com o tema de mais interesse, enquanto as organizações conseguem encontrar pessoas a partir de sua necessidade.
+
+O projeto Doe Ação é uma rede caritativa que visa promover uma cultura de solidariedade, permitindo que pessoas se envolvam em causas sociais e façam a diferença no mundo.
+
+Projeto desenvolvido durante o bootcamp da organização Generation Brasil, com objetivo de criar uma plataforma baseada na ODS(Objetivos de Desenvolvimento Sustentável) de Erradicação da Pobreza.
+
+Membros:
+
+Eduardo Nogueira
+Eliza Reis
+Matheus Sousa
+Maylis Maria
+Nathan Coelho [![Linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://github.com/nathanccoelho)
+
+Ricardo Galvão
--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,10 +1,13 @@
 import { useNavigate } from 'react-router-dom'
 
 import './Cadastro.css'
+import { useState } from 'react';
 
 function Cadastro() {
 
     const navigate = useNavigate()
+
+    const [tipoConta, setTipoConta] = useState('');
 
     function back() {
         navigate('/login')
@@ -15,21 +18,43 @@ function Cadastro() {
                 <div className="fundoCadastro hidden lg:block"></div>
                 <form className='flex justify-center items-center flex-col w-2/3 gap-3' >
                     <h2 className='text-slate-900 text-5xl'>Cadastrar</h2>
-                    
+
                     <div className="flex flex-col w-full">
                         <label htmlFor="cpf">Tipo do cadastro</label>
                         <select
+                        onChange={(e) => setTipoConta(e.target.value)}
                             id="cpf"
                             name="cpf"
                             placeholder="cpf"
                             className="border-2 border-slate-700 rounded p-2">
 
                             <option value="cpf">Pessoa Física</option>
-                            <option value="cnpj">ONG</option>
+                            <option value="ong">ONG</option>
 
-                            
                         </select>
                     </div>
+                    
+                    {tipoConta === 'cpf' && <div className="flex flex-col w-full">
+                        <label htmlFor="cpf">CPF</label>
+                        <input
+                            type="cpf"
+                            id="cpf"
+                            name="cpf"
+                            placeholder="CPF"
+                            className="border-2 border-slate-700 rounded p-2"
+                        />
+                    </div>}
+                    {tipoConta === 'ong' &&
+                        <div className="flex flex-col w-full">
+                            <label htmlFor="cnpj">CNPJ</label>
+                            <input
+                                type="cnpj"
+                                id="cnpj"
+                                name="cnpj"
+                                placeholder="CNPJ"
+                                className="border-2 border-slate-700 rounded p-2"
+                            />
+                        </div>}
 
                     <div className="flex flex-col w-full">
                         <label htmlFor="nome">Nome</label>
@@ -62,7 +87,7 @@ function Cadastro() {
                             className="border-2 border-slate-700 rounded p-2"
                         />
                     </div>
-                   
+
                     <div className="flex flex-col w-full">
                         <label htmlFor="foto">Foto</label>
                         <input
