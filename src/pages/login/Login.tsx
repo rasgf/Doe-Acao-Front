11cc7794e37c6e6ed7b5fb6a69441a12cf97f042
Ricardo <pattz.rg@hotmail.com>
2023-11-13 15:10:03 -0300

    código teste pro Geandro

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import {FormEvent, useContext } from 'react';
+import {AuthContext } from '../../contexts/AuthContext'
+
 import './Login.css';
 
 function Login() {
 
+    useContext(AuthContext)
+
+    const navigate = useNavigate()
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        navigate('/home')
+    }
     return (
         <>
         <div className= "min-h-[80vh] flex justify-center items-center">
@@ -23,7 +34,8 @@ function Login() {
                         </label>
                     </div>
                     <div className="relative h-11 w-full min-w-[200px]">
-                        <input
+                        <input 
+                        type = "password"
                             className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-rose-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                             placeholder=" "
                         />
