418df83dc7843ad948832c73a86e67e4ede1e366
Matheus Sousa <dsousamt@gmail.com>
2023-11-22 15:32:35 -0300

    cria style botao curtida, deletar e editar post

--- src/components/postagens/cardPostagens/CardPostagens.tsx
+++ src/components/postagens/cardPostagens/CardPostagens.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import Postagem from '../../../models/Postagem'
 import Curtir from '../curtirPostagem/CurtirPostagem'
+import { GithubLogo, Pencil, Trash } from '@phosphor-icons/react'
 
 interface CardPostagensProps {
     post: Postagem
@@ -10,39 +11,39 @@ function CardPostagens({ post }: CardPostagensProps) {
     return (
         <div className='flex flex-col bg-white shadow-lg rounded-lg md:mx-auto my-8 py-8 px-8 max-w-md md:max-w-2xl'>
 
-            <div className='flex items-start px-4 py-6 justify-between w-full'>
+            <div className='flex items-start py-6 justify-between w-full'>
                 <div className="flex justify-between flex-col">
                     <img src={post.usuario?.foto} className='h-12 w-12 rounded-full mb-2'
                         alt="Imagem do Usuário" />
                     <h3 className='text-lg font-bold uppercase'>{post.usuario?.nome}</h3>
                 </div>
-                <div className="flex">
+                    <div>   </div>
+                <div className="flex items-center gap-2">
 
-                    <Link to={`/editarPostagem/${post.id}`} className='rounded text-rose-500 border-rose-500 font-bold border-solid border-2 py-2 px-4
-                                            transition-all hover:shadow-lg hover:shadow-rose-500/40 active:opacity-[0.85] mr-2'>
-                        <button>Editar</button>
+                    <Link to={`/editarPostagem/${post.id}`} className='rounded text-gray-300 p-2 transition-all hover:text-rose-500 active:opacity-[0.85]'>
+                        <button><Pencil size={24} weight='bold' /></button>
                     </Link>
-                    <Link to={`/deletarPostagem/${post.id}`} className='rounded text-rose-500 border-rose-500 font-bold border-solid border-2 py-2 px-4
-                                            transition-all hover:shadow-lg hover:shadow-rose-500/40 active:opacity-[0.85]'>
-                        <button>Deletar</button>
+                    <Link to={`/deletarPostagem/${post.id}`}
+                    className='rounded text-gray-300 p-2 transition-all hover:text-rose-500 active:opacity-[0.85]'>
+                        <button><Trash size={24} weight='bold'/></button>
                     </Link>
                 </div>
 
-                <div>  <Curtir/> </div>
 
                 
                
             </div>
             <div>
 
-                <div className='p-4'>
+                <div className='py-4 gap-2'>
                     <h4 className='text-lg font-semibold uppercase'>{post.titulo}</h4>
-                    <p className='font-semibold'>Data: {new Intl.DateTimeFormat(undefined, {
+                    <p className='text-sm text-gray-400'>Data: {new Intl.DateTimeFormat(undefined, {
                         dateStyle: 'full',
                         timeStyle: 'medium',
                     }).format(new Date(post.data))}</p>
-                    <p className='font-semibold'>Tema: {post.tema?.descricao}</p>
+                    <p className='font-semibold text-rose-500'>Tema: {post.tema?.descricao}</p>
                     <p>{post.conteudo}</p>
+                    <div className='a'><Curtir/></div>
                 </div>
             </div>
         </div>
--- src/components/postagens/curtirPostagem/CurtirPostagem.tsx
+++ src/components/postagens/curtirPostagem/CurtirPostagem.tsx
@@ -1,3 +1,4 @@
+import { ThumbsUp } from '@phosphor-icons/react';
 import { useState } from 'react'
 
 function Curtir() {
@@ -9,12 +10,12 @@ function Curtir() {
 
     return (
         <div>
-            <button onClick= {Curtir} className='rounded text-rose-500 border-rose-500 font-bold border-solid border-2 py-2 px-4
-                        transition-all hover:shadow-lg hover:shadow-rose-500/40 active:opacity-[0.85]'>
-                       Curtir
-                      </button>
-            <p>Curtidas: {valor}</p>
-
+            <button onClick= {Curtir}
+            className='flex flex-row gap-2 rounded text-rose-500 border-rose-500 font-bold border-solid border-2 py-1 px-2 my-4
+            transition-all hover:shadow-lg hover:shadow-rose-500/40 active:text-rose-500 activeborder-rose-500'>
+                <ThumbsUp size={24} weight='bold' />
+                <span>{valor}</span>
+            </button>
         </div>
     )
 }
