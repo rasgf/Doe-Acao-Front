07bea91a945f5143f6562df69e80eb985a90fc12
Nathan Gabriel <nathancoelho.job@gmail.com>
2023-11-22 16:30:10 -0300

    update navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -22,15 +22,17 @@ function Navbar() {
         component = (
           <div className="relative  -mt-6  grid h-28 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-rose-500 to-rose-600 bg-clip-border text-white shadow-lg shadow-pink-500/40">
           <div className="container flex justify-between text-lg ">
-          <Link to='/home' className='hover:underline'>Doe Ação</Link>
-            <div className="flex gap-4">
-  
-            <Link to='/temas' className='hover:underline'>Temas</Link>
-            <Link to='/cadastroTema' className='hover:underline'>Cadastrar Tema</Link>
-            <Link to='/postagens' className='hover:underline'>Postagens</Link>
-            <Link to='/perfil' className='hover:underline'>Perfil</Link>
-            <Link to='/sobre-nos' className='hover:underline'>Sobre</Link>
-            <Link to="" onClick={logout} className="hover:underline">Sair</Link>
+          <Link to='/home' className=' font-bold text-3xl hover:bg-rose-600 rounded-md border-white hover:border-2 px-2 hover'>Doe Ação</Link>
+            
+            <div className="flex gap-4 font-bold">
+            
+            
+             <Link to='/temas' className='hover:bg-rose-600 rounded-md border-white hover:border-2 pr-2 pl-2 hover'>Temas</Link>
+            <Link to='/cadastroTema' className='hover:bg-rose-600 rounded-md border-white hover:border-2 px-2 hover'>Cadastrar Tema</Link>
+            <Link to='/postagens' className='hover:bg-rose-600 rounded-md border-white hover:border-2 px-2 hover'>Postagens</Link>
+            <Link to='/perfil' className='hover:bg-rose-600 rounded-md border-white hover:border-2 px-2 hover'>Perfil</Link>
+            <Link to='/sobre-nos' className='hover:bg-rose-600 rounded-md border-white hover:border-2 px-2 hover'>Sobre</Link>
+            <Link to="" onClick={logout} className='hover:bg-rose-600 rounded-md border-white hover:border-2 px-2 hover'>Sair</Link>
                     </div>
 
                 </div>
