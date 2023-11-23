6639ce10517b34892798ef35a75d990527482aa7
Eduardo Nogueira <eduardo7_@hotmail.com>
2023-11-23 02:47:06 -0300

    alteração link github footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -17,11 +17,13 @@ function Footer() {
             <div className="flex justify-center bg-gradient-to-tr from-rose-500 to-rose-600 bg-clip-border text-white shadow-lg shadow-pink-500/40 rounded-xl">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Doe Ação | &copy; {data}
+                            Doe Ação | © {data}
                         </p>
                     <p className='text-lg'>Acesse o nosso GitHub</p>
                     <div className='flex gap-2'>
-                    <GithubLogo size={48} weight='bold' />
+                        <a href='https://linktr.ee/doeacao' target='_blank' rel='noopener noreferrer'>
+                            <GithubLogo size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>)
